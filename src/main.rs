8bcaff09044e0284c2ba03f255e5e5129d84c9c5
Yangsibai <yangsibai@gmail.com>
2018-04-13 15:08:42 +0800

    simple execute on file change

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::time::SystemTime;
+use std::fs;
+use std::{thread, time};
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let fileToWatch = env::args().nth(1).expect("Please specifc the file to watch.");
+    let commandToExecute = env::args().nth(2).expect("Please specifc the command to execute.");
+    println!("{} {}", fileToWatch, commandToExecute);
+
+    let mut initialTime = getModifiedTime(&fileToWatch);
+
+    let sleepTime = time::Duration::from_secs(5);
+
+    while true {
+        let currentTime = getModifiedTime(&fileToWatch);
+        if currentTime != initialTime {
+            println!("execute the command {}", commandToExecute);
+            execute(&commandToExecute);
+            initialTime = currentTime;
+        }
+        thread::sleep(sleepTime);
+    }
+}
+
+
+fn getModifiedTime(path: &str) -> SystemTime {
+    fs::metadata(path).unwrap().modified().unwrap()
+}
+
+
+fn execute(cmd: &str) {
+    let output = Command::new(cmd).output().expect("Fail to execute process");
+    println!("{:?}", output);
 }
